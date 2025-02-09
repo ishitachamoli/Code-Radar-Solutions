24dd53babf6d453d43c66ac53feae7f94d986036
ishitachamoli <167972884+ishitachamoli@users.noreply.github.com>
2025-02-09 21:24:04 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 int main(void){
-    double a, b; 
+    int a, b;
     char ch;
     scanf("%d %d %c", &a, &b, &ch);
     if(ch=='+'){
         printf("%d", a+b);
     }
-    if(ch=='-'){
+    else if(ch=='-'){
         printf("%d", a-b);
     }
-    if(ch=='*'){
+    else if(ch=='*'){
         printf("%d", a*b);
     }
-    if(ch=='/'){
+    else if(ch=='/'){
         printf("%d", a/b);
+    }
+    else{
+        printf("Error");
     }
     return 0;
 }
